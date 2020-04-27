c48517f869b42c00813a9d5e4cd1c8c656ccc2d2
JahMaD13 <matthi85@live.fr>
2020-04-27 17:47:58 +0200

    ignore yarn.lock & symfony.lock integration remember me création du formulaire ProfilType.php

---
+++ src/Form/ProfilType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('pseudo')
+            ->add('telephone')
+            ->add('mail')
+            ->add('actif')
+            ->add('password')
+            ->add('participantSorties')
+            ->add('site')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
